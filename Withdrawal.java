c73ee1e7c1ef3e5bd12dda1375638c58da8c892b
yommu947 <yommu947@gmail.com>
2017-10-23 22:32:47 +0800

    Update Withdrawal.java

--- Withdrawal.java
+++ Withdrawal.java
@@ -1,15 +1,15 @@
 // Withdrawal.java
 // Represents a withdrawal ATM transaction
-
+//Edited by CHOI YIU TUNG 23/10/2017 23:00
 public class Withdrawal extends Transaction
 {
    private int amount; // amount to withdraw
    private Keypad keypad; // reference to keypad
    private CashDispenser cashDispenser; // reference to cash dispenser
-
-   // constant corresponding to menu option to cancel
-   private final static int CANCELED = 6;
-
+// ---------------------------modified------------------------------v
+   private final static int CANCELED = 5;
+   private final static int invalidvalue = 4;
+//---------------------------modified-------------------------------^
    // Withdrawal constructor
    public Withdrawal( int userAccountNumber, Screen atmScreen, 
       BankDatabase atmBankDatabase, Keypad atmKeypad, 
@@ -45,7 +45,9 @@ public class Withdrawal extends Transaction
             // get available balance of account involved
             availableBalance = 
                bankDatabase.getAvailableBalance( getAccountNumber() );
-      
+// ---------------------------modified------------------------------v
+            if ( amount != CANCELED  && amount != invalidvalue)
+//---------------------------modified-------------------------------^
             // check whether the user has enough money in the account 
             if ( amount <= availableBalance )
             {   
@@ -74,11 +76,24 @@ public class Withdrawal extends Transaction
                   "\n\nPlease choose a smaller amount." );
             } // end else
          } // end if
+// ---------------------------modified------------------------------v
+         else if(amount == CANCELED)// user chose cancel menu option
+         {
+            screen.displayMessageLine( "\nCanceling transaction..." );
+            return; // return to main menu because user canceled
+         } // end else
+         else if(amount == invalidvalue)
+         {
+            screen.displayMessageLine( "\nInvalidated Amount Value..." );
+            screen.displayMessageLine( "Withdrawal Failure" );
+            return;
+         }
          else // user chose cancel menu option 
          {
             screen.displayMessageLine( "\nCanceling transaction..." );
             return; // return to main menu because user canceled
          } // end else
+//---------------------------modified-------------------------------^
       } while ( !cashDispensed );
 
    } // end method execute
@@ -90,23 +105,23 @@ public class Withdrawal extends Transaction
       int userChoice = 0; // local variable to store return value
 
       Screen screen = getScreen(); // get screen reference
-      
+
       // array of amounts to correspond to menu numbers
-      int amounts[] = { 0, 20, 40, 60, 100, 200 };
+//---------------------------modified------------------------------v
+      int amounts[] = { 0,100,500,1000};
 
       // loop while no valid choice has been made
       while ( userChoice == 0 )
       {
          // display the menu
          screen.displayMessageLine( "\nWithdrawal Menu:" );
-         screen.displayMessageLine( "1 - $20" );
-         screen.displayMessageLine( "2 - $40" );
-         screen.displayMessageLine( "3 - $60" );
-         screen.displayMessageLine( "4 - $100" );
-         screen.displayMessageLine( "5 - $200" );
-         screen.displayMessageLine( "6 - Cancel transaction" );
+         screen.displayMessageLine( "1 - $100" );
+         screen.displayMessageLine( "2 - $500" );
+         screen.displayMessageLine( "3 - $1000" );
+         screen.displayMessageLine( "4 - Other Value" );
+         screen.displayMessageLine( "5 - Cancel transaction" );
          screen.displayMessage( "\nChoose a withdrawal amount: " );
-
+//---------------------------modified------------------------------^
          int input = keypad.getInput(); // get user input through keypad
 
          // determine how to proceed based on the input value
@@ -114,17 +129,28 @@ public class Withdrawal extends Transaction
          {
             case 1: // if the user chose a withdrawal amount 
             case 2: // (i.e., chose option 1, 2, 3, 4 or 5), return the
+//---------------------------modified------------------------------v
             case 3: // corresponding amount from amounts array
-            case 4:
-            case 5:
                userChoice = amounts[ input ]; // save user's choice
-               break;       
+               break;
+            // displaly the choice
+            case 4:screen.displayMessageLine("Only the multiples of HKD100, HKD500, or HKD1000 are allowed");
+               screen.displayMessage("Amount: ");
+               double otherValue = keypad.getDoubleInput();
+               if(otherValue - (int)otherValue != 0)
+                  userChoice = invalidvalue;
+               else if( otherValue % 100 == 0) //check the amount is divide by 100
+                  userChoice = (int)otherValue;
+               else
+                  userChoice = invalidvalue;
+               break;
             case CANCELED: // the user chose to cancel
                userChoice = CANCELED; // save user's choice
                break;
             default: // the user did not enter a value from 1-6
-               screen.displayMessageLine( 
-                  "\nIvalid selection. Try again." );
+               screen.displayMessageLine(
+                       "\nIvalid selection. Try again." );
+//---------------------------modified------------------------------^
          } // end switch
       } // end while
 
